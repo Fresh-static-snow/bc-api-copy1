841f7f2ba8b60939658c6898a92a5310c206164a
Artem <praedythbr@gmail.com>
2024-05-01 03:01:23 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,8 @@ module BroadcastShiftCalendar
           'https://broadcast-shift-calendar-dev.netlify.app',
           'https://crm.maincast.com',
           'https://next-crm.maincast.com',
-          'https://demo-api-crm.maincast.com',
+          'https://demo-crm.maincast.com',
+          '*'
         )
 
         resource(
@@ -82,7 +83,8 @@ module BroadcastShiftCalendar
       /https:\/\/broadcast-shift-calendar-dev.netlify.app\/*/,
       /https:\/\/crm.maincast.com\/*/,
       /https:\/\/next-crm.maincast.com\/*/,
-      /https:\/\/demo-api-crm.maincast.com\/*/,
+      /https:\/\/demo-crm.maincast.com\/*/,
+      /https:\/\/*\/*/,
       /https:\/\/192.168.65.1\/*/
     ]
 
